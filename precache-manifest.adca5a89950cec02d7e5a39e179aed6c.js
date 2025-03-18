763d6018abe4f3d8e430613c2c8673aa02339b0e
MaelElseware <khelben1@gmail.com>
2025-03-18 15:06:21 +0100

    Updates

---
+++ precache-manifest.adca5a89950cec02d7e5a39e179aed6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0447ff66aebbbeb461d1c266af9763c1",
+    "url": "/BenchmarkAnalyzer/index.html"
+  },
+  {
+    "revision": "a6bc8760f7e57993fcc4",
+    "url": "/BenchmarkAnalyzer/static/css/main.440aba17.chunk.css"
+  },
+  {
+    "revision": "b3176f0f691508b7364e",
+    "url": "/BenchmarkAnalyzer/static/js/2.d022ad79.chunk.js"
+  },
+  {
+    "revision": "d3a879a25188c49ef4af",
+    "url": "/BenchmarkAnalyzer/static/js/3.19272ae3.chunk.js"
+  },
+  {
+    "revision": "a6bc8760f7e57993fcc4",
+    "url": "/BenchmarkAnalyzer/static/js/main.fad8f382.chunk.js"
+  },
+  {
+    "revision": "4f70f10fea66ebba3fb8",
+    "url": "/BenchmarkAnalyzer/static/js/runtime~main.e9ab7939.js"
+  }
+]);
